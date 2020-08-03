1fa6398c3e107564fed207519e2c5762d0c8c5a1
Lucas Penna <35812528+lucaspenna00@users.noreply.github.com>
2020-08-03 12:31:51 -0300

    Update linear_analysis.py

--- feature_selection/linear_analysis.py
+++ feature_selection/linear_analysis.py
@@ -1,8 +1,20 @@
 import statsmodels.api as sm
 import numpy as np
 import pandas as pd
-
-def get_linear_analysis(X, Y):
+    
+def get_linear_analysis(X, Y):    
+    
+    '''
+    Input:
+    X: (set of features, pandas DataFrame)
+    Y: (set of labels)
+    
+    Return:
+    DataFrame containing the linear regression coefficients and respectives p-values.
+    
+    If P-Value is lesser than significance (usually 0.05), then we can conclude that the coefficient is not
+    useful to the regression. 
+    '''
     
     mod = sm.OLS(Y, X)
     res = mod.fit()
