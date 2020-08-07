2e2381c3869aa2990edc03490d4fd7f1abd8060b
Lucas Penna <35812528+lucaspenna00@users.noreply.github.com>
2020-08-07 19:00:01 -0300

    Update vis_utils.py

--- vis/vis_utils.py
+++ vis/vis_utils.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gaussian_kde
 
-def scatter_heat_map(x, y):
+def scatter_heat_map(x, y, title):
 
     xy = np.vstack([x,y])
     z = gaussian_kde(xy)(xy)
     plt.figure()
     plt.scatter(x, y, marker='o', c=z, edgecolor='')
+    plt.title(title)
     plt.show()
     print("Correlação de Pearson: ", np.corrcoef(x, y)[0][1])
     
