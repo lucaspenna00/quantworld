2029a50534f31e3f8ceeeb9012187969f1d740ce
Lucas Penna <35812528+lucaspenna00@users.noreply.github.com>
2020-08-12 12:32:57 -0300

    Update vis_utils.py

--- vis/vis_utils.py
+++ vis/vis_utils.py
@@ -13,3 +13,22 @@ def scatter_heat_map(x, y, title):
     plt.show()
     print("Correlação de Pearson: ", np.corrcoef(x, y)[0][1])
     print("Correlação de Spearman: ", spearmanr(x, y)[0])
+
+
+def plot_scatter_test_true(Y_pred, Y_true, experimento=None):
+    
+    x = np.arange(0, len(Y_pred))
+    
+    fig = plt.figure(figsize=(15,8))
+    
+    ax1 = fig.add_subplot()
+    
+    ax1.scatter(x, Y_pred, c='orange', marker='x', label='predicted')
+    ax1.scatter(x, Y_true, c='blue', marker='o', label='true')
+    
+    plt.title(str(experimento)+" | True and Predicted")
+        
+    plt.legend()
+    
+    plt.show()
+    
