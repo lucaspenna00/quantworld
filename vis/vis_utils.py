4426ba8a4ba9269984d5ccfb06e6caab362f22e1
Lucas Penna <35812528+lucaspenna00@users.noreply.github.com>
2020-08-14 09:49:17 -0300

    precision and recall vs thresholds curve added

--- vis/vis_utils.py
+++ vis/vis_utils.py
@@ -31,4 +31,41 @@ def plot_scatter_test_true(Y_pred, Y_true, experimento=None):
     plt.legend()
     
     plt.show()
+   
+
+def get_precision_recall_curves(Y_prob, Y_teste):
     
+    def classifier(Y_prob, threshold):
+
+    pred = []
+
+    for prob in Y_prob:
+
+        if prob >= threshold:
+
+            pred.append(1)
+
+        else:
+
+            pred.append(0)
+
+    return pred
+    
+    precisions = []
+    recalls = []
+
+    thresholds = np.linspace(0, 1, 100)
+
+    for threshold in thresholds:
+
+        Y_pred = classifier(Y_prob, threshold)
+
+        precisions.append(precision_score(Y_pred, Y_teste))
+        recalls.append(recall_score(Y_pred, Y_teste))
+
+    plt.figure()
+    plt.plot(precisions, color='orange', label='precision')
+    plt.plot(recalls, color='blue', label='recall')
+    plt.legend()
+    plt.title("Precisions and Recall vs threholds")
+    plt.show()
